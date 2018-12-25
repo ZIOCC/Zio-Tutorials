a050fb8a40cb96bdd545b845f4ba870ebd211d52
Aswana Talip <44771974+Aswana54@users.noreply.github.com>
2018-12-25 11:56:39 +0800

    Demo code
    Demo code for OLED, PsyFi32 and TOF

---
+++ Demo_Code_PsyFi32_OLED_TOF/Demo_Code_PsyFi32_OLED_TOF.ino
@@ -0,0 +1,61 @@
+/*********************************************************************
+This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+This example is for a 128x32 size display using I2C to communicate
+3 pins are required to interface (2 I2C and one reset)
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SparkFun_RFD77402_Arduino_Library.h>
+
+RFD77402 myDistance; //Hook object to the library
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+void setup()   {                
+  Serial.begin(9600);
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  // init done
+
+   if (myDistance.begin() == false)
+  {
+    Serial.println("Sensor failed to initialize. Check wiring.");
+    while (1); //Freeze!
+  }
+  Serial.println("Sensor online!");
+  
+  // Clear the buffer.
+  display.clearDisplay();
+}
+
+
+void loop() {
+ // put your main code here, to run repeatedly:
+  
+  myDistance.takeMeasurement(); //Tell sensor to take measurement
+  unsigned int distance = myDistance.getDistance(); //Retrieve the distance value
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(10,0);
+  display.clearDisplay();
+  display.print(distance);
+  display.print("mm");
+  display.display();
+}
