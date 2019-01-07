70b268ff669b365b17f5fb3751b9508db7e7b87f
Aswana Talip <44771974+Aswana54@users.noreply.github.com>
2019-01-07 11:55:29 +0800

    Update code
    Updated the code

--- Demo_Code_PsyFi32_OLED_TOF/Demo_Code_PsyFi32_OLED_TOF.ino
+++ Demo_Code_PsyFi32_OLED_TOF/Demo_Code_PsyFi32_OLED_TOF.ino
@@ -16,17 +16,19 @@ BSD license, check license.txt for more information
 All text above, and the splash screen must be included in any redistribution
 *********************************************************************/
 
-#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <SparkFun_RFD77402_Arduino_Library.h>
 
 RFD77402 myDistance; //Hook object to the library
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
-
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+//Adafruit_SSD1306 display = Adafruit_SSD1306();
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup()   {                
   Serial.begin(9600);
